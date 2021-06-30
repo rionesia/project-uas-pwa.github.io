3404b5b74aad8dd73afd1f4c10183c3b57cbef60
RIO <glinthu@gmail.com>
2021-07-01 00:30:22 +0700

    205411196 Rio Bratu Pamungkas

---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,19 @@
+<template>
+<footer class="caption">
+  <p>205411196 Rio Bratu Pamungkas</p>
+</footer>
+</template>
+
+<style lang="stylus">
+footer
+  margin: 65px auto 0
+  color: #bfbfbf
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5)
+  p
+    line-height: 1
+  a
+    color: inherit
+    text-decoration: none
+    &:hover
+      text-decoration: underline
+</style>
